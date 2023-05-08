3be4cefb7e395f838224d64e21a64bf9059cd521
Lucas Michalet <lucas.michalet@symphony.com>
2023-05-08 15:26:08 +0200

    Inherit from the same interface

--- pet-clinic-data/main/java/micha/udemy/petclinic/service/PetService.java
+++ pet-clinic-data/main/java/micha/udemy/petclinic/service/PetService.java
@@ -1,16 +1,7 @@
 package micha.udemy.petclinic.service;
 
-import micha.udemy.petclinic.model.Owner;
 import micha.udemy.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Owner owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
--- pet-clinic-data/main/java/micha/udemy/petclinic/service/VetService.java
+++ pet-clinic-data/main/java/micha/udemy/petclinic/service/VetService.java
@@ -1,16 +1,7 @@
 package micha.udemy.petclinic.service;
 
 import micha.udemy.petclinic.model.Owner;
-import micha.udemy.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Owner owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Owner, Long> {
 
 }
