64b234f3a7f8aa7836f3f8d57a8f76ae8ca81f56
Lucas Michalet <lucas.michalet@symphony.com>
2023-05-14 16:17:49 +0200

    Vet save method imrpovement

--- pet-clinic-data/src/main/java/micha/udemy/petclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/micha/udemy/petclinic/service/map/VetMapService.java
@@ -1,6 +1,8 @@
 package micha.udemy.petclinic.service.map;
 
+import micha.udemy.petclinic.model.Specialty;
 import micha.udemy.petclinic.model.Vet;
+import micha.udemy.petclinic.service.SpecialtyService;
 import micha.udemy.petclinic.service.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetMapService(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -26,6 +34,14 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (vet.getSpecialties().size() > 0){
+            vet.getSpecialties().forEach(specialty -> {
+                if(specialty.getId() == null){
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(vet);
     }
 
