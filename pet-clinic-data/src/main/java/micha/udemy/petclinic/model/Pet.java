5ee97ffa90281f1e6cf1e5174f5b2b24cefdbd55
Lucas Michalet <lucas.michalet@symphony.com>
2023-06-03 18:20:16 +0200

    Add JPA entities

--- pet-clinic-data/src/main/java/micha/udemy/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/micha/udemy/petclinic/model/Pet.java
@@ -1,13 +1,27 @@
 package micha.udemy.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDate;
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
 
     public PetType getPetType() {
         return petType;
